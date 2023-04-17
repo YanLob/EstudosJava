83c4408a14c3befc73e534a3334cb80fe69ed3d9
YanLob <yanlobate@gmail.com>
2023-04-16 23:11:05 -0300

    Update JogoDaVelha.java

--- JogoDaVelha/JogoDaVelha.java
+++ JogoDaVelha/JogoDaVelha.java
@@ -1,5 +1,10 @@
 package JogoDaVelha;
 
 public class JogoDaVelha {
-
+    public static void main(String[] args) {
+        Tabuleiro campo = new Tabuleiro();
+        boolean jogo = true;
+        string vitoria = ' ';
+        
+    }
 }
