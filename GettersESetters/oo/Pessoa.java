7b9e02e1c2bd172dcead9a90da74dcab0d3815be
YanLob <yanlobate@gmail.com>
2023-04-21 21:45:40 -0300

    adicionando getters e setters

--- GettersESetters/oo/Pessoa.java
+++ GettersESetters/oo/Pessoa.java
@@ -1,5 +1,17 @@
 package oo;
 
 public class Pessoa {
-    
+    public int idade;
+
+    Pessoa(int idade){
+        this.idade = idade;
+    }
+
+    public int GetIdade(){
+        return idade;
+    }
+
+    public void SetIdade(int novaIdade){
+        this.idade = novaIdade;
+    }
 }
--- GettersESetters/oo/PessoaTeste.java
+++ GettersESetters/oo/PessoaTeste.java
@@ -2,4 +2,10 @@ package oo;
 
 public class PessoaTeste {
     
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa(30);
+        p1.SetIdade(40);
+
+        System.out.print(p1.idade);
+    }
 }
