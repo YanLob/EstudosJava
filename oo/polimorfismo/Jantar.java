1da4622f97242b3ca50e4e8f79c0bee0afb93de9
YanLob <yanlobate@gmail.com>
2023-04-30 14:55:06 -0300

    codificando classe pessoa

--- oo/polimorfismo/Jantar.java
+++ oo/polimorfismo/Jantar.java
@@ -3,7 +3,7 @@ package polimorfismo;
 public class Jantar {
     public static void main(String[] args) {
         Arroz ingrediente1 = new Arroz(80);
-
+        
         System.out.print(ingrediente1.getPeso());
     }
 }
