28fc5ed5e89b69534400723f7d01b9e1a841c65c
YanLob <yanlobate@gmail.com>
2023-04-30 15:00:09 -0300

    codificando classe Jantar
    usei um polimorfismo sobrecarregado na classe Pessoa

--- oo/polimorfismo/Jantar.java
+++ oo/polimorfismo/Jantar.java
@@ -2,8 +2,22 @@ package polimorfismo;
 
 public class Jantar {
     public static void main(String[] args) {
-        Arroz ingrediente1 = new Arroz(80);
+        Pessoa p1 = new Pessoa(90);
+        System.out.println(p1.getPeso());
+
+        Arroz ingrediente1 = new Arroz(1.0);
+        Feijao ingrediente2 = new Feijao(2.5);
         
-        System.out.print(ingrediente1.getPeso());
+        p1.comer(ingrediente1);
+        p1.comer(ingrediente2);
+
+        System.out.println(p1.getPeso());
+
+        Sorvete sobremesa = new Sorvete(3.8);
+
+        p1.comer(sobremesa);
+        
+        System.out.println(p1.getPeso());
+        
     }
 }
