a31edd6a06672ecc61134e1b3db9c25c584e00b9
YanLob <yanlobate@gmail.com>
2023-04-30 14:44:08 -0300

    codificando classe arroz

--- oo/polimorfismo/Arroz.java
+++ oo/polimorfismo/Arroz.java
@@ -1,5 +1,22 @@
 package polimorfismo;
 
 public class Arroz {
-    
+    public int peso;
+
+    public Arroz (int peso){
+        setPeso(peso);
+    }
+
+    public int getPeso (){
+        return peso;
+    }
+
+    public void setPeso (int peso){
+        if (peso >= 0){
+            this.peso = peso;
+        }
+        else{
+            System.out.print("Peso informado menor que zero!");
+        }
+    }
 }
--- oo/polimorfismo/Jantar.java
+++ oo/polimorfismo/Jantar.java
@@ -1,5 +1,9 @@
 package polimorfismo;
 
 public class Jantar {
-    
+    public static void main(String[] args) {
+        Arroz ingrediente1 = new Arroz(80);
+
+        System.out.print(ingrediente1.getPeso());
+    }
 }
