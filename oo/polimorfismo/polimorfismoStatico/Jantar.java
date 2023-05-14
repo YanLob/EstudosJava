591d6b45f55f0c8dfe7e7c7d973ff76be8c36d24
YanLob <yanlobate@gmail.com>
2023-05-13 22:40:23 -0300

    Update Jantar.java
    instanciando comida

--- oo/polimorfismo/polimorfismoStatico/Jantar.java
+++ oo/polimorfismo/polimorfismoStatico/Jantar.java
@@ -5,17 +5,17 @@ public class Jantar {
         Pessoa p1 = new Pessoa(90);
         System.out.println(p1.getPeso());
 
-        //Arroz ingrediente1 = new Arroz(1.0);
-        //Feijao ingrediente2 = new Feijao(2.5);
+        Comida ingrediente1 = new Arroz(1.0);
+        Comida ingrediente2 = new Feijao(2.5);
         
-        //p1.comer(ingrediente1);
-        //p1.comer(ingrediente2);
+        p1.comer(ingrediente1);
+        p1.comer(ingrediente2);
 
         System.out.println(p1.getPeso());
 
-        //Sorvete sobremesa = new Sorvete(3.8);
+        Comida sobremesa = new Sorvete(3.8);
 
-        //p1.comer(sobremesa);
+        p1.comer(sobremesa);
         
         System.out.println(p1.getPeso());
         
