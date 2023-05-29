d35555549b54c5bb8119e2005f5d77bcfe58f940
YanLob <yanlobate@gmail.com>
2023-05-29 09:07:02 -0300

    aa

--- lambda/CalculoTeste3.java
+++ lambda/CalculoTeste3.java
@@ -17,16 +17,5 @@ public class CalculoTeste3 {
 
         BinaryOperator<Integer> calc = (x, y)-> { return x + y; };
         System.out.println(calc.apply(n1, n2));
-
-        System.out.println("__+_+_+_++++__+_+_++_+_");
-
-        System.out.print("Digite o primeiro valor: ");
-        int n3 = entrada.nextInt();
-
-        System.out.print("Digite o segundo valor: ");
-        int n4 = entrada.nextInt();
-
-        BinaryOperator<Integer> calc2 = (x, y)-> { return x * y; };
-        System.out.println(calc2.apply(n3, n4));
     }
 }
